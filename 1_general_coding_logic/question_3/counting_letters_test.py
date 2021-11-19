9fd7bb0973914a1a11824f5ff05d62cde98c0874
Bryony Tanner <69221453+bmt2604@users.noreply.github.com>
2021-11-19 12:14:27 +0000

    Update counting_letters_test.py

--- 1_general_coding_logic/question_3/counting_letters_test.py
+++ 1_general_coding_logic/question_3/counting_letters_test.py
@@ -3,8 +3,9 @@ from counting_letters import count_letters
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_count_letters(self):
         self.assertEqual(count_letters("Hello, world!"), "Vowels: 3, Consonants: 7")
+        
         self.assertEqual(count_letters(""), "Vowels: 0, Consonants: 0")
 
 
